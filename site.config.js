f49a87fc6c8dc260f8fa7f87d24fa8c034d29515
Caos <caosbad@gmail.com>
2021-12-04 21:23:59 +0800

    feat: init commit

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '86ccc50a882f42c28065a8287c81660a',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Caos website',
+  domain: 'caos.me',
+  author: 'Caos',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  description: 'A website about blockchain develop and investment',
+  socialImageTitle: 'Caos website',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'caosbad',
+  github: 'caosbad',
+  // linkedin: '',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
@@ -43,9 +43,10 @@ module.exports = {
   // any pages defined here will override their default URL paths
   // example:
   //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
-  pageUrlOverrides: null
+  pageUrlOverrides: {
+    '/about': '714dcffe961d48fe83a23a4d8e2bf2c9',
+    '/bar': '7a535bf53146427b81ebfa01adc4fc47',
+    '/notes': '2fe0e353b1a7407683c326900abb6c7a'
+  }
+  // pageUrlOverrides: null
 }
